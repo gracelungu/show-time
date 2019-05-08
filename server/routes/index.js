44fb29c83f1bc1a60bfd8911c63f777053f1711d
Grace Lungu <gracelungu@Graces-MacBook-Pro.local>
2019-05-08 11:54:40 +0300

    [Feature #165482728] Add swagger documentation

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import swagger from 'swagger-ui-express';
+import YAML from 'yamljs';
 import user from './user';
 
 const router = express.Router();
+
+router.use('/doc', swagger.serve, swagger.setup(YAML.load(`${__dirname}/doc.yaml`)));
 
 router.use('/auth', user);
 
